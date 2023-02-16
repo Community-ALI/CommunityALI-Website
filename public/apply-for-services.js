39252d98a7c3f1c25f9232953a193da74dc127e7
OBenjOne <bschoolland@gmail.com>
2023-02-16 10:38:28 -0800

    Change hamburger

--- public/apply-for-services.js
+++ public/apply-for-services.js
@@ -1,17 +1,3 @@
-const hamburger = document.querySelector(".navigation-hamburger");
-const menu = document.querySelector(".navigation-bar");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    menu.classList.toggle("active");
-})
-
-document.querySelectorAll(".navigation-button").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    menu.classList.remove("active");
-}))
-
-
 const form = document.getElementById('form');
 form.addEventListener('submit', function (event) {
     event.preventDefault();
