9be202776ef49b2e95db17fdd0ae462a63d0a49e
Adrean Cajigas <adrean.cajigas@gmail.com>
2023-03-22 08:41:11 -0700

    Navbar Fixes

---
+++ public/navbar.js
@@ -0,0 +1,18 @@
+// This is the HTML code for the navigation bar
+// Scripted for all the HTML pages
+
+document.write("    <nav class=\"navigation-menu\">");
+document.write("        <ul class = \"navigation-bar\"> ");
+document.write("            <li class=\"navigation-button\"> <a class=\"navigation-text\" href=\"index.html\" > Home <\/a> <\/li>");
+document.write("            <li class=\"navigation-button\"> <a class=\"navigation-text\" href=\"service-search\" > Services <\/a> <\/li>");
+document.write("            <li class=\"navigation-button\"> <a class=\"navigation-text\" id=\"applicants\" href=\"applicants.html\" > Applicants <\/a> <\/li>");
+document.write("            <li class=\"navigation-button\"> <a class=\"navigation-text\" href=\"https:\/\/www.mjc.edu\/\" > MJC <\/a> <\/li>");
+document.write("            <li class=\"navigation-button\"> <a class=\"navigation-text\" id =\"navigation-login\" href=\"login.html\" > Login <\/a> <\/li>");
+document.write("        <\/ul>");
+document.write("");
+document.write("        <div class = \"navigation-hamburger\">");
+document.write("            <span class=\"navigation-line\"> <\/span>");
+document.write("            <span class=\"navigation-line\"> <\/span>");
+document.write("            <span class=\"navigation-line\"> <\/span>");
+document.write("        <\/div>");
+document.write("    <\/nav>  ");
