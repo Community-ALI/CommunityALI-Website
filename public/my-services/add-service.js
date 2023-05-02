d260e61cdf064f707d190be500266bfc78aec453
OBenjOne <bschoolland@gmail.com>
2023-05-02 11:48:07 -0700

    Start work on app-service
    File selection and form submission detection

--- public/my-services/add-service.js
+++ public/my-services/add-service.js
@@ -1,7 +1,19 @@
 const service_form = document.getElementById('form');
 const files = document.getElementById("files");
 
+const fileInputJS2 = document.getElementById('file-input');
+const fileContainerJS2 = document.getElementById('file-container');
+// make the file input visible
+fileInputJS2.addEventListener('change', () => {
+  if (fileInput.value) {
+    fileContainerJS2.classList.add('file-selected');
+    alert('file detected');
+  } else {
+    fileContainerJS2.classList.remove('file-selected');
+  }
+});
 
+// once image is ready to send
 onLoadFunction = function(image, file){
     file = files.files[0];
     // Check if the image size is "close" to 1200 x 800
@@ -20,23 +32,23 @@ onLoadFunction = function(image, file){
     const formData = new FormData();
 
     // Append form data to the FormData object
-    formData.append("personal_name", document.getElementById("personal-name").value);
-    formData.append("personal_number", document.getElementById("personal-number").value);
-    formData.append("personal_email", document.getElementById("personal-email").value);
-    formData.append("personal_role", document.getElementById("personal-role").value);
+    // formData.append("personal_name", document.getElementById("personal-name").value);
+    // formData.append("personal_number", document.getElementById("personal-number").value);
+    // formData.append("personal_email", document.getElementById("personal-email").value);
+    // formData.append("personal_role", document.getElementById("personal-role").value);
     formData.append("title", document.getElementById("title").value);
     formData.append("details_location", document.getElementById("details-location").value);
     formData.append("details_times", document.getElementById("details-times").value);
     formData.append("details_date", document.getElementById("details-date").value);
     formData.append("description", document.getElementById("description").value);
-    formData.append("author", document.getElementById("author").value);
-    formData.append("president_email", document.getElementById("president-email").value);
-    formData.append("vice_president_name", document.getElementById("vice-president-name").value);
-    formData.append("vice_president_email", document.getElementById("vice-president-email").value);
-    formData.append("ICC_rep_name", document.getElementById("ICC-rep-name").value);
-    formData.append("ICC_rep_email", document.getElementById("ICC-rep-email").value);
-    formData.append("advisor_name", document.getElementById("advisor-name").value);
-    formData.append("advisor_email", document.getElementById("advisor-email").value);
+    // formData.append("author", document.getElementById("author").value);
+    // formData.append("president_email", document.getElementById("president-email").value);
+    // formData.append("vice_president_name", document.getElementById("vice-president-name").value);
+    // formData.append("vice_president_email", document.getElementById("vice-president-email").value);
+    // formData.append("ICC_rep_name", document.getElementById("ICC-rep-name").value);
+    // formData.append("ICC_rep_email", document.getElementById("ICC-rep-email").value);
+    // formData.append("advisor_name", document.getElementById("advisor-name").value);
+    // formData.append("advisor_email", document.getElementById("advisor-email").value);
 
 
     
@@ -70,9 +82,10 @@ onLoadFunction = function(image, file){
 }
 
 
-
+// when submit clicked
 service_form.addEventListener('submit', function (event) {
     event.preventDefault();
+    alert('form submitted');
     token = localStorage.getItem('token');
     if (token){
         // get values
