8f678f5aad97c7451eb9187fbc67ff5ed53dceb5
MasonPartridge <masonepartridge@gmail.com>
2023-05-02 21:51:46 -0700

    Add dynamic pathing to navbar links

--- public/components/navbar.js
+++ public/components/navbar.js
@@ -5,6 +5,24 @@ var token = localStorage.getItem('token');
 var currentPage = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
 var fixedPages = ['index.html', ''];
 
+var pathToRoot = './';
+
+console.log(location.pathname.lastIndexOf('/'));
+console.log(location.pathname);
+
+distanceFromPublicRoot = location.pathname;
+distanceFromPublicRoot = distanceFromPublicRoot.substring(1);
+while (distanceFromPublicRoot.lastIndexOf('/') != -1) {
+    pathToRoot = '.' + pathToRoot;
+    distanceFromPublicRoot = distanceFromPublicRoot.substring(0, location.pathname.lastIndexOf('/') - 1);
+    console.log("hello world");
+    console.log(distanceFromPublicRoot);
+    console.log(pathToRoot);
+}
+
+console.log(distanceFromPublicRoot);
+console.log(pathToRoot);
+
 function constructLoginNavButton() {
     if (!token){
         let loginNavButton = document.createElement('a');
@@ -20,7 +38,7 @@ function constructLoginNavButton() {
     else{
         let logoutNavButton = document.createElement('a');
         logoutNavButton.setAttribute('class', 'navigation-button navigation-text');
-        logoutNavButton.setAttribute('href', 'logout.html');
+        logoutNavButton.setAttribute('href', pathToRoot + 'components/logout.html');
         logoutNavButton.setAttribute('id', 'navigation-login');
         logoutNavButton.innerHTML = 'Logout';
         return logoutNavButton;
@@ -29,9 +47,9 @@ function constructLoginNavButton() {
 
 function constructCommunityCatalystLogo(isMobile) {
     let communityCatalystLogoLink = document.createElement('a');
-    communityCatalystLogoLink.setAttribute('href', 'index.html');
+    communityCatalystLogoLink.setAttribute('href', pathToRoot + 'index.html');
     let communityCatalystLogoImg = document.createElement('img');
-    communityCatalystLogoImg.setAttribute('src', 'Photos/CClogo.png');
+    communityCatalystLogoImg.setAttribute('src', pathToRoot + 'Photos/CClogo.png');
     communityCatalystLogoImg.setAttribute('class', 'navbar-logo');
     if (isMobile)
     {
@@ -54,20 +72,20 @@ function constructNavigationBarElement() {
 
     let homeNavButton = document.createElement('a');
     homeNavButton.setAttribute('class', 'navigation-button navigation-text');
-    homeNavButton.setAttribute('href', 'index.html');
+    homeNavButton.setAttribute('href', pathToRoot + 'index.html');
     homeNavButton.innerHTML = 'Home';
     navigationMenu.appendChild(homeNavButton);
 
     let serviceSearchNavButton = document.createElement('a');
     serviceSearchNavButton.setAttribute('class', 'navigation-button navigation-text');
-    serviceSearchNavButton.setAttribute('href', 'explore-services/main-page');
+    serviceSearchNavButton.setAttribute('href', pathToRoot + 'explore-services/main-page');
     serviceSearchNavButton.innerHTML = 'Explore';
     navigationMenu.appendChild(serviceSearchNavButton);
 
     if (token){
         let myServicesNavButton = document.createElement('a');
         myServicesNavButton.setAttribute('class', 'navigation-button navigation-text');
-        myServicesNavButton.setAttribute('href', 'my-services.html');
+        myServicesNavButton.setAttribute('href', pathToRoot + 'my-services/main-page.html');
         myServicesNavButton.setAttribute('id', 'applicants');
         myServicesNavButton.innerHTML = 'My Services';
         navigationMenu.appendChild(myServicesNavButton);
@@ -91,25 +109,25 @@ function constructSubNavBar() {
 
     let subjectAndCareerPathButton = document.createElement('a');
     subjectAndCareerPathButton.setAttribute('class', 'navigation-button navigation-text');
-    subjectAndCareerPathButton.setAttribute('href', 'service-search');
+    subjectAndCareerPathButton.setAttribute('href', pathToRoot + 'explore-services/main-page');
     subjectAndCareerPathButton.innerHTML = 'Subject and Career Path';
     subNavBar.appendChild(subjectAndCareerPathButton);
 
     let clubsAndCommunitiesButton = document.createElement('a');
     clubsAndCommunitiesButton.setAttribute('class', 'navigation-button navigation-text');
-    clubsAndCommunitiesButton.setAttribute('href', 'service-search');
+    clubsAndCommunitiesButton.setAttribute('href', pathToRoot + 'explore-services/main-page');
     clubsAndCommunitiesButton.innerHTML = 'Clubs and Communities';
     subNavBar.appendChild(clubsAndCommunitiesButton);
 
     let volunteerAndCommunityServiceButton = document.createElement('a');
     volunteerAndCommunityServiceButton.setAttribute('class', 'coming-soon navigation-button navigation-text');
-    clubsAndCommunitiesButton.setAttribute('href', 'service-search');
+    clubsAndCommunitiesButton.setAttribute('href', pathToRoot + 'explore-services/main-page');
     volunteerAndCommunityServiceButton.innerHTML = 'Volunteer & Community Service';
     subNavBar.appendChild(volunteerAndCommunityServiceButton);
 
     let internshipAndWorkExperienceButton = document.createElement('a');
     internshipAndWorkExperienceButton.setAttribute('class', 'coming-soon navigation-button navigation-text');
-    clubsAndCommunitiesButton.setAttribute('href', 'service-search');
+    clubsAndCommunitiesButton.setAttribute('href', pathToRoot + 'explore-services/main-page');
     internshipAndWorkExperienceButton.innerHTML = 'Internship & Work Experience';
     subNavBar.appendChild(internshipAndWorkExperienceButton);
 
