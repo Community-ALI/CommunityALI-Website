407a49faee76e5fed846b1f17698e00d0ae49857
MasonPartridge <masonepartridge@gmail.com>
2023-05-05 23:49:02 -0700

    Add Webpack and Babel.js to website
    Deleted most of the website temporarily and will add them later after converting the front end to use React.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import LandingPage from './pages/LandingPage/LandingPage';
+
+createRoot(document.getElementById('root')).render(<LandingPage />);
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+    entry: './src/index.js',
+
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                        }
+                }
+            },
+            {
+                test: /\.mp4$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "video"
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ["style-loader", "css-loader"]
+            }       
+        ]
+    },
+
+    
+
+}
