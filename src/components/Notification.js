a0ac0113ab14ca3c44673ea6b0f812553c2e628d
MasonPartridge <masonepartridge@gmail.com>
2023-05-28 09:15:04 -0700

    Add notifications component

---
+++ src/components/Notification.js
@@ -0,0 +1,17 @@
+import '../../public/stylesheets/style.css'
+import React from 'react';
+
+function Notifications(props) {
+    console.log(props.notifications);
+    if (props.notifications > 0){
+        return (
+            <div
+                className='notification-icon'
+            >{props.notifications}</div>
+        )
+    } else {
+        return null;
+    }
+}
+
+export default Notifications;
