159f777fcc8773c55c2257c07caab9bcfc56e943
OBenjOne <bschoolland@gmail.com>
2023-06-01 09:09:01 -0700

    Service deletion popup frontend

---
+++ src/components/DeleteServicePopup.js
@@ -0,0 +1,117 @@
+import React, { Component, useEffect, useState } from 'react';
+import './login.css';
+
+function DeleteServicePopup(props) {
+
+    const [usernameOrEmail, setUsernameOrEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+        async function deleteService(event) {
+            event.preventDefault();
+            const validEmailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+            let authData;
+            if (validEmailRegex.test(usernameOrEmail)) {
+                authData = { usernameOrEmail, password }
+            } else {
+                authData = { usernameOrEmail, password }
+            }
+
+            
+            try {
+                const response = await fetch('http://localhost:3000/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(authData),
+                });
+                const result = await response.json();
+
+                if (result.status === 'ok') {
+                    //everything is a okay
+                    console.log('Got the token: ', result.data);
+                    var token = result.data;
+                    const response = await fetch(`http://localhost:3000/delete-service?service=${props.serviceTitle}`,
+                        {
+                        method: 'POST',
+                        headers: {
+                        'Authorization': `Bearer ${token}`
+                        }
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                    alert('yay.')
+                    })
+                    if (!response.ok) {
+                        throw new Error('Failed to delete service');
+                    }
+                    else{
+                        window.location.href = '/explore';
+                    }
+                } else {
+                    console.log(result.error);
+                    alert(result.error);
+                }
+            } catch (error) {
+                console.error(error);
+                alert('An error ocurred.');
+            }
+        }
+
+    // enterPassword(enteredPassword) {
+
+    // }
+
+    if (props.isShowingServiceDeletePopup) {
+        return (
+                <div className='container-login'>
+                    <div className="container-for-login">
+                        <h1>Are you sure you want to delete "{props.serviceTitle}"?</h1>
+                        <form id="login" onSubmit={deleteService}>
+                            <div className="text-field">
+                                <input 
+                                    className="text-field-input"
+                                    id="usernameOrEmail" 
+                                    required=""
+                                    onChange={e => {
+                                        setUsernameOrEmail(e.target.value);
+                                    }}
+                                    />
+                                <span className="text-field-span"></span>
+                                <label className="text-field-label">Username or Email</label>
+                            </div>
+                            <div className="text-field">
+                                <input 
+                                    className="text-field-input"
+                                    type="password" 
+                                    id="password" 
+                                    required=""
+                                    onChange={e => {
+                                        setPassword(e.target.value);
+                                    }}
+                                    />
+                                <span className="text-field-span" ></span>
+                                <label className="text-field-label">Password</label>
+                            </div>
+                    
+                            {/* <!-- <div class="pass">Forgot Password?</div> --> */}
+                            <input 
+                                type="submit" 
+                                value="Delete" 
+                                id='login-submission'
+                            />
+                            <div className="signup_link">
+                                {/* left this for spacing */}
+                            </div>
+                        </form>
+                    </div>
+                </div>
+        )
+    }
+
+    return null;
+}
+
+export default DeleteServicePopup;
--- src/pages/my-services/main-page.js
+++ src/pages/my-services/main-page.js
@@ -2,81 +2,63 @@ import React, { Component, useState, useEffect } from "react";
 import './main-page.css';
 import '../../../public/stylesheets/style.css'
 import NavBar from '../../components/NavBar';
+import DeleteServicePopup from '../../components/DeleteServicePopup'
 // create the information required to display the page
 
-const handleDeleteService = async (title) => {
-  try {
-    var token = localStorage.getItem("token");
-    alert(`are you sure you want to delete ${title} ?`);
-    const response = await fetch(`http://localhost:3000/delete-service?service=${title}`,
-        {
-          method: 'POST',
-          headers: {
-        'Authorization': `Bearer ${token}`
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-      alert('yay.')
-    })
-      
-
-    if (!response.ok) {
-      throw new Error('Failed to delete service');
-    }
-
-    // Service deleted successfully, handle the response here
-
-  } catch (error) {
-    console.error(error);
-    // Handle error cases here
-  }
-};
 
 
-function MyServicePageDisplay(props) {
-    const service = props.service;
-  
-    const handleBackgroundClick = () => {
-      window.location.href = 'view-applicants?service='+service.title;
-    };
-     
-    return (
-      <div className="user-service" onClick={handleBackgroundClick}>
-        <div 
-            className={
-                ' notification-icon' + 
-                (props.notificaitons < 1) ? ' hidden' : ''
-            }
-        >{props.notificaitons}</div>
-        <div className="option-container-service">
-          <a className="user-link" href={`view-applicants?service=${service.title}`}>
-            <i className="fa-solid fa-users fa-2x"></i>
-          </a>
-          <a className="user-service-text" href={`view-applicants?service=${service.title}`}>
-            {service.title}
-          </a>
-          <a
-            className="edit-button"
-            href={`edit-service?service=${service.title}`}
-          >Edit</a>
-          <button
-            className="service-delete-button"
-            onClick={(event) => {
-              event.stopPropagation(); // Prevent event bubbling
-              handleDeleteService(service.title);
-            }}
-          >Delete</button>
-        </div>
-      </div>
-    );
-  }
+
 
 function MyServicesHome() {
- 
+
     const [services, setServices] = useState([]);
     const [username, setUsername] = useState('no username associated with token')
-    const [notificaitons, setNotifications] = useState(0); // TODO connect this with backend
+    const [notificaitons, setNotifications] = useState(1); // TODO connect this with backend
+    const [isShowingServiceDeletePopup, setIsShowingServiceDeletePopup] = useState(false);
+    const [deleteServiceTitle, setDeleteServiceTitle] = useState('');
+
+
+    function MyServicePageDisplay(props) {
+    
+      const service = props.service;
+    
+      const handleBackgroundClick = () => {
+        window.location.href = 'view-applicants?service='+service.title;
+      };
+       
+      return (
+        <div className="user-service" onClick={handleBackgroundClick}>
+          <div 
+              className={
+                  ' notification-icon' + 
+                  (props.notificaitons < 1) ? ' hidden' : ''
+              }
+          >{props.notificaitons}</div>
+          <div className="option-container-service">
+            <a className="user-link" href={`view-applicants?service=${service.title}`}>
+              <i className="fa-solid fa-users fa-2x"></i>
+            </a>
+            <a className="user-service-text" href={`view-applicants?service=${service.title}`}>
+              {service.title}
+            </a>
+            <a
+              className="edit-button"
+              href={`edit-service?service=${service.title}`}
+            >Edit</a>
+            <button
+              className="service-delete-button"
+              onClick={(event) => {
+                event.stopPropagation(); // Prevent event bubbling
+                setDeleteServiceTitle(service.title);
+                setIsShowingServiceDeletePopup(true);
+              }}
+            >Delete</button>
+          </div>
+        </div>
+      );
+    }
+
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -124,6 +106,14 @@ function MyServicesHome() {
             <span className="loader"><span className="loader-inner"></span></span>
         </div>,
         <NavBar isFixedPage={false} notificaitons={notificaitons} />,
+        <DeleteServicePopup isShowingServiceDeletePopup={isShowingServiceDeletePopup} serviceTitle={deleteServiceTitle}/>
+        <div 
+            id='login-popup-background' 
+            className={isShowingServiceDeletePopup ? '' : 'hidden'}
+            onClick={() => setIsShowingServiceDeletePopup(false)}
+            style={{cursor:'pointer'}}
+            >
+        </div>
         <div className = "username-title">
             {"Welcome: " + username}
         </div>
