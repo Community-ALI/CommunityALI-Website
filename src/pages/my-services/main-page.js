0c0e6b3603063b887653f1d7d38947acba5135e9
MasonPartridge <masonepartridge@gmail.com>
2023-06-02 14:13:11 -0700

    Fix merge conflicts

--- src/pages/my-services/main-page.js
+++ src/pages/my-services/main-page.js
@@ -44,31 +44,79 @@ function MyServicePageDisplay(props) {
     console.log(applicants);
   })
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        var token = localStorage.getItem('token');
+        if (token) {
+          console.log('sending request');
+          const response = await fetch('http://localhost:3000/get-user-services',
+            {
+              headers: {
+                'Authorization': `Bearer ${token}`
+              }
+            })
+            .then(response => response.json())
+            .then(data => {
+              // 'data' variable will contain the received object with the data array and tokenUsername
+
+              setServices(data.dataServices);
+              setUsername(data.tokenUsername);
+              const loaderWrapper = document.querySelector(".loader-wrapper");
+              loaderWrapper.style.transition = "opacity 0.5s";
+              loaderWrapper.style.opacity = "0";
+              setTimeout(() => {
+                loaderWrapper.style.display = "none";
+              }, 500); // fade out duration in milliseconds
+            })
+        }
+        else {
+          console.log('no token found')
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log('services: ', services);
+  }, [services]);
+
   const handleBackgroundClick = () => {
     window.location.href = 'view-applicants?service=' + service.title;
   };
 
   return (
-    <div className="user-service" onClick={handleBackgroundClick}>
+    <div className="user-service-container">
+      <div className="user-service" onClick={handleBackgroundClick}>
       <Notifications notifications={notifications} />
-      <div className="option-container-service">
-        <a className="user-link" href={`view-applicants?service=${service.title}`}>
-          <i className="fa-solid fa-users fa-2x"></i>
-        </a>
-        <a className="user-service-text" href={`view-applicants?service=${service.title}`}>
-          {service.title}
-        </a>
-        <a
-          className="edit-button"
-          href={`edit-service?service=${service.title}`}
-        >Edit</a>
-        <button
-          className="service-delete-button"
-          onClick={(event) => {
-            event.stopPropagation(); // Prevent event bubbling
-            handleDeleteService(service.title);
-          }}
-        >Delete</button>
+
+        <div className="option-container-service">
+          <div className="user-link-container">
+            <a className="user-link" href={`view-applicants?service=${service.title}`}>
+              <i className="fa-solid fa-users fa-2x"></i>
+            </a>
+            <a className="user-service-text" href={`view-applicants?service=${service.title}`}>
+              {service.title}
+            </a>
+          </div>
+
+          <div className="edit-delete-container">
+            <a className="edit-button" href={`edit-service?service=${service.title}`}
+            >Edit</a>
+            <button className="service-delete-button"
+              onClick={(event) => {
+                event.stopPropagation(); // Prevent event bubbling
+                // window.location.href = 'https://www.youtube.com/watch?v=oHC1230OpOg'
+                setDeleteServiceTitle(service.title);
+                setIsShowingServiceDeletePopup(true);
+              }}
+            >Delete</button>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -78,98 +126,8 @@ function MyServicesHome() {
   // create the information required to display the page
   const [services, setServices] = useState([]);
   const [username, setUsername] = useState('no username associated with token')
-  const [notificaitons, setNotifications] = useState(1); // TODO connect this with backend
   const [isShowingServiceDeletePopup, setIsShowingServiceDeletePopup] = useState(false);
   const [deleteServiceTitle, setDeleteServiceTitle] = useState('');
-
-
-  function MyServicePageDisplay(props) {
-
-    const service = props.service;
-
-    const handleBackgroundClick = () => {
-      window.location.href = 'view-applicants?service=' + service.title;
-    };
-
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          var token = localStorage.getItem('token');
-          if (token) {
-            console.log('sending request');
-            const response = await fetch('http://localhost:3000/get-user-services',
-              {
-                headers: {
-                  'Authorization': `Bearer ${token}`
-                }
-              })
-              .then(response => response.json())
-              .then(data => {
-                // 'data' variable will contain the received object with the data array and tokenUsername
-
-                setServices(data.dataServices);
-                setUsername(data.tokenUsername);
-                const loaderWrapper = document.querySelector(".loader-wrapper");
-                loaderWrapper.style.transition = "opacity 0.5s";
-                loaderWrapper.style.opacity = "0";
-                setTimeout(() => {
-                  loaderWrapper.style.display = "none";
-                }, 500); // fade out duration in milliseconds
-              })
-          }
-          else {
-            console.log('no token found')
-          }
-        } catch (error) {
-          console.log(error)
-        }
-      };
-
-      fetchData();
-    }, []);
-
-    useEffect(() => {
-      console.log('services: ', services);
-    }, [services]);
-
-    return (
-      <div className="user-service-container">
-        <div className="user-service" onClick={handleBackgroundClick}>
-          <div
-            className={
-              ' notification-icon' +
-                (props.notificaitons < 1) ? ' hidden' : ''
-            }
-          >{props.notificaitons}
-          </div>
-
-          <div className="option-container-service">
-            <div className="user-link-container">
-              <a className="user-link" href={`view-applicants?service=${service.title}`}>
-                <i className="fa-solid fa-users fa-2x"></i>
-              </a>
-              <a className="user-service-text" href={`view-applicants?service=${service.title}`}>
-                {service.title}
-              </a>
-            </div>
-
-            <div className="edit-delete-container">
-              <a className="edit-button" href={`edit-service?service=${service.title}`}
-              >Edit</a>
-              <button className="service-delete-button"
-                onClick={(event) => {
-                  event.stopPropagation(); // Prevent event bubbling
-                  // window.location.href = 'https://www.youtube.com/watch?v=oHC1230OpOg'
-                  setDeleteServiceTitle(service.title);
-                  setIsShowingServiceDeletePopup(true);
-                }}
-              >Delete</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
 
   return (
     <div>
@@ -202,8 +160,7 @@ function MyServicesHome() {
       {services.map(service => ( // display each service
         <MyServicePageDisplay
           key={service._id}
-          service={service}
-          notificaitons={notificaitons} />
+          service={service} />
       ))}
     </div >
   )
