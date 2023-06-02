c1a7356a9748723f295da2fd32a7d68bb2879361
MasonPartridge <masonepartridge@gmail.com>
2023-06-02 11:36:49 -0700

    Change export name

--- get-functions/get-all-user-notifications.js
+++ get-functions/get-all-user-notifications.js
@@ -5,7 +5,7 @@ const Applications = models.Application;
 const Services = models.Services;
 
 // access database, call functions, display page
-const get_service_applicants_notifications = async function (username) {
+const get_all_user_notifications = async function (username) {
     try {
         const selected_services = await Services.find({ user: username }).exec();
         const serviceTitles = selected_services.map(service => service.title);
@@ -18,4 +18,4 @@ const get_service_applicants_notifications = async function (username) {
 }
 
 
-module.exports = get_service_applicants_notifications;
+module.exports = get_all_user_notifications;
