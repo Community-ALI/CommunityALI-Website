7d9467809c07c3a0d34f349f5a92f52cb86e8346
Adrean Cajigas <adrean.cajigas@gmail.com>
2023-06-06 11:08:11 -0700

    Update get-all-services.js

--- get-functions/get-all-services.js
+++ get-functions/get-all-services.js
@@ -1,18 +1,18 @@
-// // database
-// const models = require("../connect-to-database");
-// const Services = models.Services;
+// database
+const models = require("../connect-to-database");
+const Services = models.Services;
 
 
-// // get all services from database
-// const get_services = async function () {
-//   try {
-//     foundServices = await Services.find().exec();
-//     return foundServices;
-//   } catch (error) {
-//     console.log(error);
-//     return { success: false, error: 'internal database error' };
-//   }
-// }
+// get all services from database
+const get_services = async function () {
+  try {
+    foundServices = await Services.find().exec();
+    return foundServices;
+  } catch (error) {
+    console.log(error);
+    return { success: false, error: 'internal database error' };
+  }
+}
 
-// // export to app.js 
-// module.exports = get_services;
+// export to app.js 
+module.exports = get_services;
