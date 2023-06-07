f83717aa928e4bb82e30aa14bc39c817ce943a91
MasonPartridge <masonepartridge@gmail.com>
2023-06-07 12:29:04 -0700

    Optimize get all user notifications

--- get-functions/get-all-user-notifications.js
+++ get-functions/get-all-user-notifications.js
@@ -7,7 +7,7 @@ const Services = models.Services;
 // access database, call functions, display page
 const get_all_user_notifications = async function (username) {
     try {
-        const selected_services = await Services.find({ user: username }).exec();
+        const selected_services = await Services.find({ user: username }).select('title').exec();
         const serviceTitles = selected_services.map(service => service.title);
         const applicants = await Applications.find({ is_new_applicant: true, service: { $in: serviceTitles } }).exec();
         return applicants;
