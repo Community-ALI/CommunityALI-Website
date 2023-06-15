4e22afcb5ac3d69b155c0fce43d6104d82aa2b34
MasonPartridge <masonepartridge@gmail.com>
2023-06-14 23:23:05 -0700

    Implement new information page format

--- src/pages/explore-services/service-info-pages/club-info.js
+++ src/pages/explore-services/service-info-pages/club-info.js
@@ -4,8 +4,10 @@ import '../../../components/navbar.css';
 import '../service-info.css';
 import ContactsPage from "./contacts-page";
 import OverviewPage from "./overview-page";
-import FaqPage from "../general-components/faq-page";
+// import FaqPage from "../general-components/faq-page";
 import SignUpPage from "./sign-up-page";
+import '../../../pages/my-services/add-service.css';
+import '../../../../public/stylesheets/style.css';
 
 function AddClub() {
   const allPossiblePages = [
@@ -15,12 +17,12 @@ function AddClub() {
     "Sign Up"
   ];
 
+  const [allCurrentPages, setAllCurrentPages] = useState([
+    "Overview",
+    "Sign Up"
+  ]);
 
   const [activePage, setActivePage] = useState("Overview");
-
-  const [overviewFormData, setOverviewFormData] = useState({});
-  const [contactsFormData, setContactsFormData] = useState({});
-  const [faqFormData, setFaqFormData] = useState({});
 
   const pageRefs = useRef(allPossiblePages.reduce((refs, page) => {
     refs[page] = useRef(null);
@@ -36,69 +38,38 @@ function AddClub() {
     });
   };
 
-  
   useEffect(() => {
     changeVisibility("Overview"); // Set "Overview" as the active page initially
   }, []);
 
-
   return (
     <div>
       <NavBar isFixedPage={false} />
-      <form method="POST" className="service-container" id='form'>
+      <div className="py-32">
         <div className="service-title">
-          <input type="text" placeholder="Name of the Club" className="club-title-text-box" name="title" id='title' ref={titleRef} onChange={() => setTitleValue(titleRef.current.value)} /><br />
+          <h1 className="club-title-text-box" >Name of the Club</h1>
+          <br />
         </div>
 
         <div className="service-navbar">
           {allCurrentPages.map((page) => (
             <div className="service-navbar-text" key={page} id={page === "OverviewPage" ? "" : `remove${page}`}>
               <a href="#" className="service-navbar-link" ref={pageRefs.current[page]} onClick={() => changeVisibility(page)}>{page}</a>
-              {removablePages.includes(page) && <i className="fa-solid fa-circle-xmark" id="remove-service-navbar-text" onClick={() => deletePage(page)}></i>}
             </div>
           ))}
 
           <div
-        className="service-navbar-plus-container"
-        onClick={toggleAddButtons}
-        onBlur={hide}
-        tabIndex="0"
-      >
-        <i className="fa-solid fa-circle-plus fa-2x" id="service-navbar-plus">
-        {showAddButtons && (
-          <div className="add-buttons-container">
-            {allPossiblePages
-              .filter((page) => !allCurrentPages.includes(page))
-              .map((page) => (
-                <button
-                  key={page}
-                  onClick={() => addPage(page)}
-                  className="add-page-button"
-                >
-                  Add {page}
-                </button>
-              ))}
+            className="service-navbar-plus-container"
+            tabIndex="0"
+          >
           </div>
-        )}
-        </i>
-      </div>
         </div>
 
-        {activePage === "Overview" && <OverviewPage key="OverviewPage" formData={overviewFormData} setFormData={setOverviewFormData} />}
-        {activePage === "Contacts" && <ContactsPage key="ContactsPage" formData={contactsFormData} setFormData={setContactsFormData} />}
-        {activePage === "FAQ" && <FaqPage key="FaqPage" formData={faqFormData} setFormData={setFaqFormData} />}
-        {activePage === "Sign Up" && <SignUpPage key="SignUpPage" mainInfo={
-          {'title': titleValue}
-        } 
-        allFormData={
-          {
-            'Overview': overviewFormData,
-            'Contacts': contactsFormData,
-            'FAQ': faqFormData
-          }
-          } />}
-
-      </form>
+        {activePage === "Overview" && <OverviewPage key="OverviewPage" />}
+        {activePage === "Contacts" && <ContactsPage key="ContactsPage" />}
+        {activePage === "FAQ" && <FaqPage key="FaqPage" />}
+        {activePage === "Sign Up" && <SignUpPage key="SignUpPage" />}
+      </div>
     </div>
   );
 }
--- src/pages/explore-services/service-info-pages/sign-up-page.js
+++ src/pages/explore-services/service-info-pages/sign-up-page.js
@@ -1,163 +1,58 @@
-import React, {useState} from "react";
-import '../add-service.css';
-
-function SignUpPage({mainInfo, allFormData}) {
-    const [file, setFile] = useState(null);
-    console.log(allFormData)
-    const checkRequired = () => {
-      if (
-        !mainInfo.title) {
-        alert('Please provide a club name');
-        return false;
-      }
-
-      // Check required fields for Overview
-      if (
-        !allFormData.Overview.subtitle ||
-        !allFormData.Overview.time ||
-        !allFormData.Overview.date ||
-        !allFormData.Overview.location ||
-        !allFormData.Overview.description ||
-        !allFormData.Overview.file
-      ) {
-        alert('Overview is incomplete');
-        return false;
-      }
-    
-      // Check required fields for Contacts
-      if (allFormData.Contacts.contacts) {
-        const contacts = allFormData.Contacts.contacts;
-    
-        // Check required fields for each contact
-        const contactsComplete = contacts.every(
-          (contact) =>
-            contact.contactRole && contact.contactName && contact.contactEmail
-        );
-    
-        if (!contactsComplete) {
-          alert('Contacts are incomplete');
-          return false;
-        }
-      }
-      if (allFormData.Contacts.socialMedia){
-        // Check required fields for social media
-        const socialMedia = allFormData.Contacts.socialMedia
-        const socialMediaComplete = socialMedia.every(
-          (media) => media.mediaType && media.mediaName && media.mediaUrl
-        );
-    
-        if (!socialMediaComplete) {
-          alert('Social media is incomplete');
-          return false;
-        }
-      }
-      if (allFormData.FAQ.faq){
-        const FAQ = allFormData.FAQ.faq;
-
-        const faqComplete = FAQ.every(
-          (faq) =>
-          faq.faqQuestion && faq.faqAnswer
-        );
-    
-        if (!faqComplete) {
-          alert('faq is incomplete');
-          return false;
-        }
-      }
-      return true;
-    };
-
-      
-
-      
+import React, { Component, useEffect, useState, useRef } from 'react';
 
 
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      if (checkRequired()){
-        const selectedFile = allFormData.Overview.file;
-        
-        if (selectedFile) {
-          const sendFormData = new FormData();
-          sendFormData.append('title', mainInfo.title);
-          sendFormData.append('image', selectedFile);
-          const OverviewData = {
-            'subtitle': allFormData.Overview.subtitle,
-            'time': allFormData.Overview.time,
-            'date': allFormData.Overview.date,
-            'location': allFormData.Overview.location,
-            'description': allFormData.Overview.description
-          }
-          const JsonData = {
-            'overview': OverviewData
-          }
-          const jsonString = JSON.stringify(JsonData);
-          // Alternatively, if you want to treat it as a file, you can create a File object
-          // const jsonFile = new File([jsonString], 'data.json', { type: 'application/json' });
+function SignUpPage({ mainInfo, allFormData }) {
 
-          // Append the JSON blob or file to the FormData
-          sendFormData.append('pages', jsonString);
-          const token = localStorage.getItem('token');
-          fetch('http://localhost:3000/upload-service', {
-            method: 'POST',
-            body: sendFormData,
-            headers: {
-              'Authorization': `Bearer ${token}`
-            },
-          })
-            .then(response => response.json())
-            .then(data => {
-              // Handle response from the server
-              console.log('Upload successful:', data);
-            })
-            .catch(error => {
-              // Handle error
-              console.error('Upload failed:', error);
-            });
-        }
-      }
-      else{
-        
-      }
-    }
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
 
-    return(
-        <div>
-          <img id="previewImage" src={file} alt="Preview"></img>
-            <div className="sign-up-form">
-              <div id="form" className="sign-up-form-boxes">
-                <div className="service-header" id="sign-up-header">Become a New Club Member Today!</div>
-                  <div className="sign-up-form-container">
-                      <div className="text-container" id="name-container">
-                          <label htmlFor="name" className="sign-up-form-text"> Full Name: </label>
-                          <input type="text" className="sign-up-form-input" placeholder="First and Last Name" 
-                          id="name" name="name" readOnly/><br/>
-                      </div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-                      <div className="text-container" id="email-container">
-                          <label htmlFor="email" className="sign-up-form-text"> Email: </label>
-                          <input type="email" className="sign-up-form-input" placeholder="School Email" 
-                          id="email" name="email" readOnly/><br/>
-                      </div>
-                  </div> 
-                  <input placeholder="Submit" className="sign-up-submit-button" readOnly/><br/>
-              </div>
+    const formData = new FormData();
+    const searchParams = new URLSearchParams(window.location.search);
+    formData.append("service", searchParams.get('service'));
+
+    formData.append("name", nameRef.current.value);
+    formData.append("email", emailRef.current.value);
+
+    fetch("http://localhost:3000/store-application", {
+      method: 'POST',
+      body: formData
+    })
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        window.location.href = '/signup-success'
+      })
+      .catch(error => {
+        console.log(error);
+
+      });
+  }
+
+  return (
+    <div>
+      <div className="sign-up-form">
+        <form onSubmit={handleSubmit} method="POST" id="form" className="sign-up-form-boxes">
+          <div className="service-header" id="sign-up-header">Become a New Club Member Today!</div>
+          <div className="sign-up-form-container">
+            <div className="text-container" id="name-container">
+              <label htmlFor="name" className="sign-up-form-text"> Full Name: </label>
+              <input className="service-info-text text-black" type="text" placeholder="First and Last Name" id="name" name="name" ref={nameRef} required /><br />
             </div>
 
-            <div className="authorization-container">
-                <label htmlFor="authorization" className="authorization-text">
-                By submitting this application I authorize Community ALI to publish all the information of this application for anybody
-                to view. I hold responsibility for the information displayed from this application and I represent the club's interest to do so.
-                I understand that this application will be subject to review, in which any inappropriate content associated with the club or its
-                members will result in immediate termination of the club from the platform. I understand that there is no tolerance for any discrimination
-                against race, religion, sex or gender, sexual orientation, ethnicity, or disability within Community ALI.
-                </label>
-                {/* <input type="submit" value="Save Application" id="save-button" className="application-buttons"> */}
-                <input type="button" value="Save Draft" id="draft-button" className="application-buttons" />
-                <input type="submit" value="Submit Application" id="submit-button" className="application-buttons" onClick={handleSubmit} />
+            <div className="text-container" id="email-container">
+              <label htmlFor="email" className="sign-up-form-text"> Email: </label>
+              <input className="service-info-text text-black" type="email" placeholder="School Email" id="email" name="email" ref={emailRef} required /><br />
             </div>
-        </div>
-    )
+          </div>
+          <input type="submit" value="Submit" className="sign-up-submit-button" /><br />
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default SignUpPage;
