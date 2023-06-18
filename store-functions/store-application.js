9747f2a7092f8ffa08725fdec28b362a1bd01459
MasonPartridge <masonepartridge@gmail.com>
2023-06-17 19:11:08 -0700

    Fix email notification TargetArn

--- store-functions/store-application.js
+++ store-functions/store-application.js
@@ -25,7 +25,13 @@ function sendNotification(req, user) {
   console.log("User:", user);
   const messageParams = {
     Message: `Subject: ${req.body.service} - New Applicants\n\n${req.body.name} has signed up to your service: ${req.body.service}`,
-    TargetArn: `arn:aws:sns:us-west-2:944066005674:endpoint/email/${user.email}`,
+    TargetArn: `arn:aws:sns:us-west-2:944066005674:serviceApplicantSNS`,
+    MessageAttributes: {
+      user_id: {
+        DataType: "String",
+        StringValue: user._id.toString(),
+      }
+    }
   };
 
   sns.publish(messageParams, (err, data) => {
