35330f79b05f265f429e81646e0f5d5409f4be75
Adrean Cajigas <adrean.cajigas@gmail.com>
2023-06-21 08:31:03 -0700

    Last fixes before pushing to Heroku

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
     return (
         <BrowserRouter>
+
             <Routes>
  
                 {/* explore services pages */}
