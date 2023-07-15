31da6c73bd60cefead43abcd48789cf7d72ba150
MasonPartridge <masonepartridge@gmail.com>
2023-07-15 10:25:45 -0700

    Set up controller for filtering categories

--- controllers/service-data.js
+++ controllers/service-data.js
@@ -10,7 +10,7 @@ function search(keyword, attribute, dbServices, filteredData) {
     if (text.includes(keyword.toLowerCase())) {
       // don't create miltiple copies of the same result if the keyword appears twice
       if (filteredData.includes(service)) {
-        
+
       }
       else {
         filteredData.push(service)
@@ -20,7 +20,7 @@ function search(keyword, attribute, dbServices, filteredData) {
   return filteredData;
 }
 
-const find_filter_service = async function (sortingType, serviceType, fields) {
+const find_filter_service = async function (sortingType, serviceType, fields, categories) {
   try {
     serviceTypes = serviceType.split(',');
     console.log(`filterType: ${serviceTypes}, sortingType: ${sortingType}`);
@@ -33,12 +33,15 @@ const find_filter_service = async function (sortingType, serviceType, fields) {
         sort = 1;
     }
     if (serviceTypes.includes('all')) {
-      services = await Services.find()
+      const services = await Services.find()
         .select(fields)
         .sort({ title: sort })
         .exec();
     } else {
-      services = await Services.find({ serviceType: { $in: serviceTypes } })
+      const services = await Services.find({
+        serviceType: { $in: serviceTypes },
+        categories: { $in: categories }
+      })
         .select(fields)
         .sort({ title: sort })
         .exec();
@@ -51,10 +54,10 @@ const find_filter_service = async function (sortingType, serviceType, fields) {
 }
 
 // get all services from database
-const get_services = async function (keywords, fields, sortingType, serviceType) {
+const get_services = async function (keywords, fields, sortingType, serviceType, categories) {
   try {
     filteredData = [];
-    foundServices = find_filter_service(sortingType, serviceType, fields);
+    foundServices = find_filter_service(sortingType, serviceType, fields, categories);
 
     filterAttribute = 'title';
     if (keywords != undefined) {
