47a7d4d1471c2b3d2865d9aeaa67472fe73d9318
OBenjOne <bschoolland@gmail.com>
2023-07-24 11:19:35 -0700

    Fix: Make sad face look less like a bug

--- src/NotFound.js
+++ src/NotFound.js
@@ -4,7 +4,7 @@ import "./NotFound.css";
 import NavBar from "./components/NavBar";
 
 const getRandomSadFace = () => {
-  const sadFaces = [":(", "D:", ":'(", ":o", ":O", "):", "),:", ":/", '=(', ":-(",":["];
+  const sadFaces = [":(", "D:", ":'(", "):", "),:", ":/", '=(', ":-(",":[", "/:"];
   const randomIndex = Math.floor(Math.random() * sadFaces.length);
   return sadFaces[randomIndex];
 };
@@ -14,14 +14,12 @@ const NotFound = () => {
   const sadFace = getRandomSadFace();
 
   return (
-    <div>
+    <div className="not-found-page-container">
       <NavBar isFixedPage={false} />
-      <p>{sadFace}</p> {/* this is absolutely neccesary! Contact Ben before removing! */}
-      
       <div className="page-not-found-container">
         <h1 className="not-found-heading">404 - Page Not Found</h1>
         <p className="not-found-message">
-          The page at <code className="not-found-code">{location.pathname}</code> does not exist.
+          The page at <code className="not-found-code">{location.pathname}</code> does not exist {sadFace}
         </p>
         <p className="not-found-message">Please check the URL you entered and try again.</p>
         <div className="go-back-from-not-found">
