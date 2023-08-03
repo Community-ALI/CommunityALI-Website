2a91425f7fb8a52baa1bea7c3b3315456428c64f
MasonPartridge <masonepartridge@gmail.com>
2023-08-02 17:16:54 -0700

    Feature: Connect applicant router to controller functions

--- routes/applicant.js
+++ routes/applicant.js
@@ -3,10 +3,11 @@ const express = require("express");
 const router = express.router;
 
 const applicant_data = require("../controllers/applicant-data");
+const service_data = require("../controllers/service-data");
 
 router.post('/change_notification_status/:id', upload.none(), async function (req, res) {
     try {
-        const message = await change_notification_status(req);
+        const message = await applicant_data.change_notification_status(req);
         if (message == 'success') {
             console.log('application from ', req.body.name, ' is not longer new');
             res.setHeader("Content-Type", "application/json");
@@ -24,7 +25,7 @@ router.post('/change_notification_status/:id', upload.none(), async function (re
 // Store user form data from an application in the database
 app.post('/store-application', upload.none(), async function (req, res) {
     try {
-        const message = await store_application(req);
+        const message = await applicant_data.store_application(req);
         if (message == 'success') {
             console.log('application from ', req.body.name, ' submitted');
             res.setHeader("Content-Type", "application/json");
@@ -43,14 +44,14 @@ app.post('/store-application', upload.none(), async function (req, res) {
 app.get("/get-service-applicants", async function (req, res) {
     try {
       const service_name = req.query.service;
-      const service = await get_one_service(service_name);
+      const service = await service_data.get_one_service(service_name);
   
       const token = req.headers.authorization.split(' ')[1];
       const decodedToken = jwt.verify(token, JWT_SECRET);
       const username = decodedToken.username;
       if (service && username && username == service.user){
         // the user owns this service
-        const applicants = await get_service_applicants(service.title);
+        const applicants = await applicant_data.get_service_applicants(service.title);
         res.json(applicants);
         console.log(applicants.length, 'applications sent for', service.title)
       }
