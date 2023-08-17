d54d0ad8d6ef60d10952229230642488b89ff4e1
MasonPartridge <masonepartridge@gmail.com>
2023-08-17 14:44:17 -0700

    Feature: Add entityManagementSelection

--- src/components/messager/entityManagementSelection.js
+++ src/components/messager/entityManagementSelection.js
@@ -1,37 +1,84 @@
 import React from "react";
 
-function EntityManagementButton() {
-  entity = props.entity;
+function EntityManagementButton(props) {
+  const entity = props.entity;
 
   return (
-    <button className="flex" onClick={props.SelectUser}>
-      <img src={entity.image} alt="photos-optimized/user-pic.png" />
-      <div className="flex flex-col">
-        <h1>{entity.name}</h1>
-        <div className="text-[#465985]">
-            <div>{entity.subtext}</div>
+    <div className="relative text-white px-4 py-2">
+      <button
+        className="bg-[#ECAA1E] absolute right-4 top-[50%] translate-y-[-50%]"
+        onClick={props.OnSettingsClick}
+      >
+        <img src="/Photos/Setting_fill.png" className=" max-h-[100%] " />
+      </button>
+      <button className="flex gap-8" onClick={props.SelectUser}>
+        <img className="w-[82px]" src={entity.image} />
+        <div className="flex flex-col">
+          <h1>{entity.name}</h1>
+          <div className="text-[#465985]">
+            <p className="text-white">{entity.subtext}</p>
+          </div>
         </div>
-      </div>
-    </button>
+      </button>
+    </div>
   );
 }
 
 function EntityList(props) {
-    if (props.areUsers) {
-        return(
-            <div className=""></div>
-        )
-    }
+  console.log(props.entities);
+  if (props.entityType == "user") {
+    return (
+      <div>
+        {props.entities.map((user) => {
+          return (
+            <EntityManagementButton
+              entity={{
+                image: user.profile,
+                name: user.fullName,
+                subtext: [user.email],
+              }}
+              key={user._id}
+            />
+          );
+        })}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {props.services.map((service) => {
+          return (
+            <EntityManagementButton
+              entity={{
+                image: service.image,
+                name: service.title,
+                subtext: [],
+              }}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default function EntityManagementSelection(props) {
+  console.log(props.entities);
   return (
-    <div>
+    <div className="max-w-[600px] bg-[#00468D]">
       <div>
-        <button>Back</button>
-        <input type="search"></input>
+        <button className="w-[10%]">Back</button>
+        <div className="w-[90%] bg-transparent">
+          <input
+            placeholder="Search"
+            type="search"
+          />
+          <img src="Photos/search.png" alt="" />
+        </div>
       </div>
-      <div></div>
+      <div>
+        <EntityList entityType={props.entityType} entities={props.entities} />
+      </div>
     </div>
   );
 }
--- src/pages/my-services/memberManagement.js
+++ src/pages/my-services/memberManagement.js
@@ -1,9 +1,31 @@
 import React from "react";
+import NavBar from "../../components/NavBar";
+import EntityManagementSelection from "../../components/messager/entityManagementSelection";
+import MessagingUI from "../../components/messager/messagingUI";
 
 export default function MemberManagement() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const service = {
+    title: "Dev Service",
+    users: [
+      {
+        fullName: "David Locke",
+        email: "davylockssupportsLGTBQ@gmail.com",
+        _id: 3214,
+        profile: "Photos/UserTemplateImage.png",
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <NavBar />
+      <div className="mt-24">
+        <EntityManagementSelection
+          entityType={"user"}
+          entities={service.users}
+        />
+        <MessagingUI />
+      </div>
+    </div>
+  );
 }
