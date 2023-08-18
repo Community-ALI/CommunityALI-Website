c2194dca4331114d677981298549b0aa10b3944a
MasonPartridge <masonepartridge@gmail.com>
2023-08-18 07:48:52 -0700

    Feature: Implement messagingUI frontend

--- src/components/messager/messagingUI.js
+++ src/components/messager/messagingUI.js
@@ -1,9 +1,38 @@
 import React from "react";
 
-export default function MessagingUI() {
-    return (
-        <div>
-            
-        </div>
-    )
+function Message(props) {
+  const message = props.message;
+  return (
+    <div>
+      <p>{message.timePosted}</p>
+      <div className="bg-[#001E60] text-white p-4">
+        <h1 className="mb-6">{message.header + ":"}</h1>
+        <p>{message.body}</p>
+      </div>
+    </div>
+  );
+}
+
+function MessageForm(props) {
+  return (
+    <div className="bg-[#bg-[#001E60]">
+      <input type="text" />
+    </div>
+  );
+}
+
+export default function MessagingUI(props) {
+  return (
+    <div>
+      <div className="">
+        <h1>{props.serviceTitle}</h1>
+      </div>
+      <div>
+        {/* <Message /> */}
+      </div>
+      <div>
+        <MessageForm />
+      </div>
+    </div>
+  );
 }
