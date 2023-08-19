692ed754015d07ed87efd36f95ba11254aa88da7
MasonPartridge <masonepartridge@gmail.com>
2023-08-19 09:54:44 -0700

    Feature: Add get service messages route

---
+++ routes/message.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const message_data = require('../controllers/message-data');
+
+//TODO: Make get service messages route
+router.get('get-service-messages/:serviceId', async function(req, res) {
+    serviceId = req.params.serviceId;
+    try {
+        const messages = await message_data.get_service_messages(serviceId);
+        console.log(`Sending ${serviceId}'s messages`);
+    }
+})
+
+module.exports = router;
