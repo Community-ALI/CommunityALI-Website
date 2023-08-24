7a79b778943acd7d435f28cba0364887d80740b4
Adrean Cajigas <adrean.cajigas@gmail.com>
2023-08-23 21:57:07 -0700

    Chore: Fix Private Policy

--- src/components/PrivacyPopUp.js
+++ src/components/PrivacyPopUp.js
@@ -3,7 +3,8 @@ import React from "react";
 // make a react component that contains a privacy policy pop up which you can scroll through
 export default function PrivacyPopUp(props) {
   return (
-    <div className="fixed z-50 top-[20vh] left-[50vw] p-8 bg-white rounded-3xl flex flex-col translate-x-[-50%] inset-0 overflow-y-auto w-[50vw] lr:w-[80vw] lr:h-[75vh] h-[50vh]">
+    <div className="fixed z-50 top-[15vh] left-[50vw] p-8 bg-white rounded-3xl flex 
+    flex-col translate-x-[-50%] inset-0 overflow-y-auto w-[60vw] h-[60vh] lr:w-[70vw] md:top-[15vh] md:w-[80vw] md:h-[65vh] sm:p-6 sm:w-[90vw] ">
       <div className="flex flex-col flex-grow">
         <iframe className="flex-grow" src="/privacyPolicy.html" />
         {!(
@@ -15,13 +16,13 @@ export default function PrivacyPopUp(props) {
             <div className="text-ali-darkblue gap-4 flex pt-4">
               <button
                 onClick={props.AcceptPrivacyPolicy}
-                className="bg-ali-orange rounded-md p-1 font-bold"
+                className="bg-ali-orange rounded-md px-2 py-1 font-semibold hover:bg-[color:var(--secondary-color)] hover:text-[white] transition duration-300 ease-out"
               >
                 Accept
               </button>
               <button
                 onClick={props.DeclinePrivacyPolicy}
-                className="bg-ali-orange rounded-md p-1 font-bold"
+                className="bg-ali-orange rounded-md px-2 py-1 font-semibold hover:bg-[color:var(--secondary-color)] hover:text-[white]  transition duration-300 ease-out"
               >
                 Decline
               </button>
