9cea0231bf564a8bac63d9dbbacabe0f77e21a62
MasonPartridge <masonepartridge@gmail.com>
2023-08-29 14:58:30 -0700

    Feature: Add filter tags component

---
+++ src/components/servicesDisplay/FilterTags.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+
+function Tag(props) {
+  return (
+    <button
+      onClick={() => {
+        props.removeTag(props.tag);
+      }}
+      className="text-white bg-ali-darkblue rounded-md flex gap-3 items-center p-1"
+      key={props.tag}
+    >
+      <p>{props.tag}</p>
+      <i className="fa-solid fa-x text-white"></i>
+    </button>
+  );
+}
+
+export default function FilterTags(props) {
+  return (
+    <div className="flex flex-wrap gap-3 px-[25px]">
+      {!props.serviceTypeFilter.includes("all") && (
+        <Tag
+          tag={props.serviceTypeFilter}
+          removeTag={() => {
+            props.SetServiceTypeFilter("all");
+          }}
+        />
+      )}
+      {props.categoriesFilter.map((tag) => {
+        if (!props.categoriesFilter.includes("all")) {
+          return (
+            <Tag
+              key={tag}
+              tag={tag}
+              removeTag={() => {
+                props.SetCategoriesFilter(
+                  props.categoriesFilter.length == 1
+                    ? ["all"]
+                    : props.categoriesFilter.filter((cat) => cat != tag)
+                );
+              }}
+            />
+          );
+        }
+      })}
+    </div>
+  );
+}
