58ca3a7a72a10e650370a94c18ead56145c8b6bc
MasonPartridge <masonepartridge@gmail.com>
2023-09-01 12:40:31 -0700

    Feature: Add loading screen when fetching services in explore

--- src/components/loading/LoadingUI.js
+++ src/components/loading/LoadingUI.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function LoadingUI() {
     return (
-        <div className="flex justify-center items-center z-50">
+        <div className="flex justify-center items-center w-[100%] h-[100%] z-50">
           <div className="w-[30px] h-[30px] border-[4px] border-white animate-ali-spin">
             <div className="bg-white w-[100%] h-[100%] animate-ali-fill origin-top"></div>
           </div>
