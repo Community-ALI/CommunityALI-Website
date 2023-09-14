151a96eb3a0cbc017357ab270fdc61527cef705d
MasonPartridge <masonepartridge@gmail.com>
2023-09-14 03:20:49 -0700

    Fix: users GET API

---
+++ api/users/controllers.js
@@ -0,0 +1,11 @@
+const { rejects } = require("assert");
+const database = require("../../connect-to-database.js");
+const User = database.User;
+
+exports.GET = async function () {
+  try {
+    return await User.find().exec();
+  } catch (error) {
+    throw error;
+  }
+};
---
+++ api/users/routes.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+const controllers = require("./controllers.js");
+
+router.get("/", async function (req, res) {
+  try {
+    const users = await controllers.GET();
+    res.json({ users: users });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
